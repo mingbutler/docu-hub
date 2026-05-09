fbb3e757b9773713a4a9d2e1cab69e480a1cb4eb
Ming Butler <Minglibutler@gmail.com>
2026-05-08 23:44:54 -0400

    Add supabase vector store

---
+++ backend/app/services/vector_store.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+import getpass
+from typing import cast
+
+from langchain_community.vectorstores import SupabaseVectorStore
+from langchain_openai import OpenAIEmbeddings
+from supabase.client import Client, create_client
+
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("OpenAI API Key:")
+    
+if "SUPABASE_URL" not in os.environ:
+    os.environ["SUPABASE_URL"] = getpass.getpass("Supabase URL:")
+    
+if "SUPABASE_SERVICE_KEY" not in os.environ:
+    os.environ["SUPABASE_SERVICE_KEY"] = getpass.getpass("Supabase Service Key:")
+    
+
+load_dotenv()
+
+supabase_url = cast(str, os.environ.get('SUPABASE_URL'))
+supabase_key = cast(str, os.environ.get('SUPABASE_SERVICE_KEY'))
+openai_api_key = cast(str, os.environ.get('OPENAI_API_KEY'))
+
+supabase: Client = create_client(supabase_url, supabase_key)
+embeddings = OpenAIEmbeddings(model='text-embedding-3-small')
+
+vector_store = SupabaseVectorStore(
+    client=supabase,
+    embedding=embeddings,
+    table_name='documents',
+    query_name='match_documents'
+)
